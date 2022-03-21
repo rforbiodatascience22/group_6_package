5e99c9e188c63316df33934db04be521e16d7be2
Joachim <joachim.breitenstein@hotmail.com>
2022-03-21 11:52:16 +0000

    .

---
+++ R/codon_iden.R
@@ -0,0 +1,22 @@
+#' Codon identifier
+#'
+#' Identifies codons from inputted RNA sequence. 
+#'
+#' @format Function
+#'
+#' @param RNA_seq 
+#' @param start 
+#'
+#' @return
+#' @export
+#'
+codon_iden <- function(RNA_seq, start = 1){ 
+  len_seqs <- nchar(RNA_seq) #count characters in input string
+  codons <- substring(RNA_seq,
+                      first = seq(from = start, to = len_seqs-3+1, by = 3),
+                      last = seq(from = 3+start-1, to = len_seqs, by = 3)) #count number of codons in input string
+  return(codons) 
+}
+
+
+
