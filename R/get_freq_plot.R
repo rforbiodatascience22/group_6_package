b041a82fbc8fbf3895d7a9ec4b800a0b67dcdf42
EsbenOyan <s194564@dtu.dk>
2022-03-21 12:35:11 +0000

    Function 5 complete

---
+++ R/get_freq_plot.R
@@ -0,0 +1,35 @@
+#'Generate frequency plot
+#'
+#'Takes amino acid sequence as input, counts instances of amino acids and 
+#'generates a frequency plot.
+#'
+#' @param aa_seq An Amino acid sequence as string.
+#' 
+#' @return A frequency plot
+#' @export
+#'
+#' @importFrom magrittr %>%
+get_freq_plot <- function(aa_seq){
+  
+  # Generates a list of unique amino acids present in input string.
+  aa_unique <- aa_seq %>%
+    stringr::str_split(pattern = stringr::boundary("character"), simplify = TRUE) %>%
+    as.character() %>%
+    unique()
+  
+  # Counts instances of unique amino acids.
+  counts <- sapply(aa_unique, function(aa_seq) stringr::str_count(string = aa_seq, pattern = aa_unique)) %>% 
+    as.data.frame()
+  
+  # Change column and row names in data frame.
+  colnames(counts) <- c("Counts")
+  counts[["Amino_acid"]] <- rownames(counts)
+  
+  # Generate frequency plot using ggplot2.
+  freq_plot <- counts %>% 
+    ggplot2::ggplot(ggplot2::aes(x = Amino_acid, y = Counts, fill = Amino_acid)) +
+    ggplot2::geom_col() +
+    ggplot2::theme_bw()
+  
+  return(freq_plot)
+}
