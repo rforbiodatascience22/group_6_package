c31e025474e217239329c92fe578ab4a77a953c0
WilliamH-R <s194545@dtu.dk>
2022-03-21 09:00:19 +0000

    Test function created

---
+++ R/test_function.R
@@ -0,0 +1,14 @@
+#' Title
+#'
+#' @param foo 
+#' @param bar 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+myFunction <- function(foo, bar){
+  # Do stuff with foo and bar
+  foobar <- (foo * bar) / (foo + bar)
+  return(foobar)
+}
